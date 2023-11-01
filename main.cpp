0f2fd0f943f8ca45858f456c18a3b272088db0a3
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2023-11-01 12:31:49 +0900

    プルリクエストの作成

--- main.cpp
+++ main.cpp
@@ -3,15 +3,15 @@
 int main() {
 	
 	printf("modified...\n");
-	pritnf("LC1A_19_ﾅｲﾄｳ_ｿｳﾄ\n");
+	printf("LC1A_19_ﾅｲﾄｳ_ｿｳﾄ\n");
 
 	printf("player作成\n");
 
 	printf("マージしてください。\n");
 
-  printf("まじでマージします！\n);
+	printf("まじでマージします！\n");
 
-  printf("プルリクエストの作成\n);
+	printf("プルリクエストの作成\n");
 
 	return 0;
 }
